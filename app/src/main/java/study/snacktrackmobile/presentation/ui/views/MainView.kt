fa855c36fa6fba5bfe2751b5b107dfad7e01857f
BartoszPilka <bartoszpilka02@gmail.com>
2025-11-05 15:09:27 +0100

    Fixed MainView

--- app/src/main/java/study/snacktrackmobile/presentation/ui/views/MainView.kt
+++ app/src/main/java/study/snacktrackmobile/presentation/ui/views/MainView.kt
@@ -1,18 +1,11 @@
 package study.snacktrackmobile.presentation.ui.views
 
-import android.app.Activity
-import androidx.activity.compose.BackHandler
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavController
 
 @Composable
 fun MainView(navController: NavController)
 {
-    val activity = LocalContext.current as Activity
-
-    BackHandler {
-        activity.finish()
-    }
+    Text("MainView :DDDDDDDDDD")
 }
